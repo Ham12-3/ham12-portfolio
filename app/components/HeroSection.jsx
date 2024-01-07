a00f10b2bb6163d5fb9df58f879501da78b16c46
Ham12-3 <mobolaji2309@gmail.com>
2024-01-07 14:35:55 +0000

    'update'

--- app/components/HeroSection.jsx
+++ app/components/HeroSection.jsx
@@ -48,7 +48,7 @@ const HeroSection = () => {
             </Link>
             <Link
             download
-            href='./Abdulhamid_Sonaike_-_Full_Stack_Developer.pdf'
+            href='./Abdulhamid_Sonaike_-_Full_Stack_Engineer,_Software_Engineer,_AWS_Cloud_Engineer.pdf'
               className="px-1 inline-block py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-primary-500 to-secondary-500 hover:bg-slate-800 text-white mt-3"
             >
               <span  className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">
