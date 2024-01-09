fd3281c1b03e092ed3d80368ae7ae75ae1121ede
Ham12-3 <mobolaji2309@gmail.com>
2024-01-09 00:02:44 +0000

    'update'

--- app/components/HeroSection.jsx
+++ app/components/HeroSection.jsx
@@ -24,11 +24,11 @@ const HeroSection = () => {
               sequence={[
                 "Abdulhamid",
                 1000,
-                "Full-stack Developer",
+                "Full-Stack Engineer",
                 1000,
-                "Mobile Developer",
+                "Mobile App Engineer",
                 1000,
-                "Blockchain developer",
+                "Cloud Engineer",
                 1000,
               ]}
               wrapper="span"
@@ -63,13 +63,15 @@ const HeroSection = () => {
           transition={{ duration: 0.5 }}
           className="col-span-4 place-self-center mt-4 lg:mt-0"
         >
-          <div className="rounded-full  w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative">
+          <div className="">
             <Image
               src='/images/abdulhamid-nkan.jpg'
               alt="hero image"
-              className="rounded-full absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
-              width={200}
-              height={200}
+              className=""
+              width={500}
+              height={500}
+              style={{borderRadius:'50%', objectFit:'cover', clipPath: 'circle()'}}
+              
             />
           </div>
         </motion.div>
@@ -79,3 +81,8 @@ const HeroSection = () => {
 };
 
 export default HeroSection;
+
+
+// absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2 
+
+// w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative 
